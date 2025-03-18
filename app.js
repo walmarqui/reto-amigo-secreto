7eebf95e604e90a532a704d94fefcd8e253164d4
Walmart <walmarqui@gmail.com>
2025-03-18 09:24:40 -0600

    funcion agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Array para guardar la lista de amigos
+let listaAmigos = [];
+
+// funcion para agregar los amigos a la lista
+function agregarAmigo(){
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor ingrese un nombre valido");
+    }
+    
+    //Agregar nombre a la lista
+    listaAmigos.push(nombre);
+
+    //Limpiar el input
+    input.value = "";
+
+    //Actualizar la lista en la pantalla
+    mostrarLista();
+}
+
+//Funcion para mostrar la lista de amigos
+function mostrarLista() {
+    const 
+}
